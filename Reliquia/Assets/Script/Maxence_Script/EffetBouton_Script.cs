82ba39936c813f0666b5231fb70402c66334f3d4
BlastMx <mxcolas302@gmail.com>
2020-05-28 22:28:19 +0200

    Couleur Bouton Hom
    Changement couleur bouton home lors du hover

--- Reliquia/Assets/Script/Maxence_Script/EffetBouton_Script.cs
+++ Reliquia/Assets/Script/Maxence_Script/EffetBouton_Script.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EffetBouton_Script : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
 {
     [SerializeField] private bool changementCouleurText = true;
+    Scene scene;
 
+    void Awake()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (changementCouleurText) gameObject.GetComponentInChildren<Text>().color = new Color32(105,105,105,255);
+        if (changementCouleurText)
+        {
+            if(scene.name == "Menu_00") gameObject.GetComponentInChildren<Text>().color = new Color32(198,150,82,255);
+            else if(scene.name == "Menu_01") gameObject.GetComponentInChildren<Text>().color = new Color32(105,105,105,255);
+        }
     }
     public void OnPointerExit(PointerEventData eventData)
     {
