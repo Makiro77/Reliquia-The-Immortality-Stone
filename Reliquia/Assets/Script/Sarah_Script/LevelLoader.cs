da056eddd38ecc7b22ec9ae8630fe254ae87773e
SarahBailly <bailly-sarah@outlook.fr>
2020-09-20 23:49:44 +0200

    Ecrans de chargement
    Modifications du script et de la scène concernés

--- Reliquia/Assets/Script/Sarah_Script/LevelLoader.cs
+++ Reliquia/Assets/Script/Sarah_Script/LevelLoader.cs
@@ -6,16 +6,15 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour {
 
-    public GameObject canvas1;
-    public GameObject canvas2;
-    public GameObject canvas3;
-    public GameObject loadingScreen;
-    public Slider slider;
+    public GameObject loadingScreen1; // Ecran de chargement à afficher
+    public GameObject loadingScreen2; // Second écran de chargement
+    public GameObject amulette; // Amulette remplie
+    public Slider slider; // Slider de chargement
 
     public void Start () {
         
-        canvas2.SetActive(false);
-        canvas3.SetActive(false);
+        loadingScreen1.SetActive(false); 
+        loadingScreen2.SetActive(false); 
     }
 
     public void LoadLevel (int sceneIndex) {
@@ -25,17 +24,34 @@ public class LevelLoader : MonoBehaviour {
 
     IEnumerator LoadAsynch (int sceneIndex) {
 
+        /// Le code ci dessous est présent uniquement pour l'aperçu ///
+
+        loadingScreen1.SetActive(true);
+
+        for(float i=0;i<1.1f;i+=.1f){
+            slider.value = i;
+            amulette.GetComponent<CanvasGroup>().alpha = i;
+            Debug.Log(i);
+            yield return new WaitForSeconds(.1f);
+        }
+
+        SceneManager.LoadScene(0);
+
+        /// La véritable fonction prenant en compte le temps de chargement du niveau est en commentaire ci-dessous ///
+
+        /*
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-        loadingScreen.SetActive(true);
+        loadingScreen1.SetActive(true);
 
         while (!operation.isDone) {
             
             float progress = Mathf.Clamp01(operation.progress / .9f);
             
             slider.value = progress;
+            Debug.Log(progress);
 
             yield return null;
-        }
+        }*/
     }
 }
