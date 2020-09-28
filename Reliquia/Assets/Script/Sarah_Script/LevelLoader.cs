3ee2dfd098dce1521d9ae433dc3a08927f6df862
SarahBailly <bailly-sarah@outlook.fr>
2020-09-28 12:44:04 +0200

    Modifications - Pages de chargement
    Remplacement des images de fond, ajout de texte Unity et de l'animation "Chargement..."

--- Reliquia/Assets/Script/Sarah_Script/LevelLoader.cs
+++ Reliquia/Assets/Script/Sarah_Script/LevelLoader.cs
@@ -9,7 +9,7 @@ public class LevelLoader : MonoBehaviour {
     public GameObject loadingScreen1; // Ecran de chargement à afficher
     public GameObject loadingScreen2; // Second écran de chargement
     public GameObject amulette; // Amulette remplie
-    public Slider slider; // Slider de chargement
+    public Text loadingPoints; // "Chargement..."
 
     public void Start () {
         
@@ -24,31 +24,42 @@ public class LevelLoader : MonoBehaviour {
 
     IEnumerator LoadAsynch (int sceneIndex) {
 
-        /// Le code ci dessous est présent uniquement pour l'aperçu ///
+        /// Le code ci-dessous est présent uniquement pour l'aperçu ///
         
         loadingScreen1.SetActive(true);
 
         for(float i=0;i<1.1f;i+=.1f){
-            slider.value = i;
+
+            if (loadingPoints.text.Length == 13)
+                loadingPoints.text = "Chargement";
+
+            else
+                loadingPoints.text = loadingPoints.text + ".";
+
             amulette.GetComponent<CanvasGroup>().alpha = i;
             Debug.Log(i);
-            yield return new WaitForSeconds(.1f);
+            yield return new WaitForSeconds(.4f);
         }
 
         SceneManager.LoadScene(0);
 
         /// La véritable fonction prenant en compte le temps de chargement du niveau est en commentaire ci-dessous ///
-
         /*
+        
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
         loadingScreen1.SetActive(true);
 
         while (!operation.isDone) {
+
+            if (loadingPoints.text.Length == 13)
+                loadingPoints.text = "Chargement";
+
+            else
+                loadingPoints.text = loadingPoints.text + ".";
             
             float progress = Mathf.Clamp01(operation.progress / .9f);
             
-            slider.value = progress;
             amulette.GetComponent<CanvasGroup>().alpha = progress;
             Debug.Log(progress);
 
