215340f52de9c34150d1b95e27025e6ec33ed13a
SarahBailly <bailly-sarah@outlook.fr>
2020-11-21 15:36:55 +0100

    Corrections animations "Lighting"
    Tests et corrections

---
+++ Reliquia/Assets/Script/Sarah_Script/Powers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powers : MonoBehaviour
+{
+    public Animator _animator;
+    
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+   
+    public void Update()
+    {
+        if (Input.GetKey(/*raccourciClavier.toucheClavier["Pouvoir 1"]*/KeyCode.E)) {
+
+             _animator.SetBool("Lighting", true);
+        }
+    }
+}
