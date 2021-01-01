4098de733bc317beda103e316704085b863cacfd
SarahBailly <bailly-sarah@outlook.fr>
2021-01-01 21:55:33 +0100

    Animations "Lighting" + Directions

--- Reliquia/Assets/Script/Sarah_Script/Powers.cs
+++ Reliquia/Assets/Script/Sarah_Script/Powers.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class Powers : MonoBehaviour
 {
     public Animator _animator;
-    
+
+
     void Start()
     {
         _animator = GetComponent<Animator>();
     }
    
     public void Update()
-    {
+    {   
+
         if (Input.GetKey(/*raccourciClavier.toucheClavier["Pouvoir 1"]*/KeyCode.E)) {
 
-             _animator.SetBool("Lighting", true);
+            _animator.SetBool("Lighting", true);
+        }
+
+        if (Input.GetKey(/*raccourciClavier.toucheClavier["Pouvoir 1"]*/KeyCode.R)) {
+
+            _animator.SetBool("Lighting", false);
         }
     }
 }
